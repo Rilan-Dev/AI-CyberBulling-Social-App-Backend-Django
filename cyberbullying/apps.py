34ad4d8e5c24da6c861da16669484f4eadbbc952
Rilan-Dev <mohammedrilan.ksa@gmail.com>
2026-03-20 02:17:08 +0530

    cdeo updats

--- cyberbullying/apps.py
+++ cyberbullying/apps.py
@@ -5,8 +5,4 @@ from django.apps import AppConfig
 class CyberbullyingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cyberbullying'
-
-    def ready(self):
-        # Import here to avoid circular imports
-        from .ml_views import ready_model
-        ready_model()
+
